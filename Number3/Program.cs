9fa26f4a96a322ecce9a597357c8282422f5a62a
Gleb <gleb.gadbrat@mail.ru>
2022-03-07 23:07:32 +0300

    Задание 3 - Дано число обозначающее день недели. Выяснить является номер дня недели выходным

---
+++ Number3/Program.cs
@@ -0,0 +1,15 @@
+﻿void Day(int date)
+{
+    if (date == 6 ||date == 7)
+    {
+        Console.WriteLine("Сегодня выходной. Можешь отдыхать.");
+    }
+    else
+    {
+        Console.WriteLine("Сегодня будни. Пора работать.");
+    }
+}
+
+Console.WriteLine("Введите число дня недели: ");
+int Today = Convert.ToInt32(Console.ReadLine());
+Day(Today);

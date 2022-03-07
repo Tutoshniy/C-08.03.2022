89d8d0f07e39ada5afb2b34c3da41e30442d9a2d
Gleb <gleb.gadbrat@mail.ru>
2022-03-07 23:40:28 +0300

    Задание 5 - Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X != 0 и Y != 0

---
+++ Number5/Program.cs
@@ -0,0 +1,13 @@
+﻿void Opredelenie(int X, int Y)
+{
+    if (X > 0 && Y > 0) Console.WriteLine("Точка находится в первой координатной плоскости");
+    if (X < 0 && Y > 0) Console.WriteLine("Точка находится во второй координатной плоскости");
+    if (X < 0 && Y < 0) Console.WriteLine("Точка находится в третьей координатной плоскости");
+    if (X > 0 && Y < 0) Console.WriteLine("Точка находится в четвёртой координатной плоскости");
+}
+Console.WriteLine("Введите координату точки по оси абсцисс/X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки по оси ординат/Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Opredelenie(x, y);

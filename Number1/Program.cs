a3c6cf7425b695873eee81aa6b751dbd43402c6f
Gleb <gleb.gadbrat@mail.ru>
2022-03-07 22:53:51 +0300

    Задание 1 и 2 - Показать числа от -N до N и Показать четные числа от 1 до N

---
+++ Number1/Program.cs
@@ -0,0 +1,16 @@
+﻿void ShowNumbers(int a, int b)
+{
+    while (a <= b)
+    {
+        Console.Write(" ");
+        Console.Write(a);
+        a++;
+    }
+}
+
+Console.WriteLine("Введите Начальное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Конечное число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Результат: ");
+ShowNumbers(a, b);
---
+++ Number2/Program.cs
@@ -0,0 +1,18 @@
+﻿void ChetOrNechet (int Last)
+{
+    int CurrentNumber = 1;
+    while (CurrentNumber <= Last)
+    {
+        if (CurrentNumber % 2 == 0)
+        {
+            Console.Write(" ");
+            Console.Write(CurrentNumber);
+        }
+        CurrentNumber++;
+    }
+}
+
+Console.WriteLine("Введите конечное число: ");
+int End = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Найдены Чётные числа: ");
+ChetOrNechet(End);

e6fadf347f03357fbfb8724c3397d8bf7b0cb492
Gleb <gleb.gadbrat@mail.ru>
2022-03-07 23:24:08 +0300

    задача 4 - По двум заданным числам проверять является ли одно квадратом другого

---
+++ Number4/Program.cs
@@ -0,0 +1,14 @@
+﻿void Proverka(int a, int b)
+{
+    if (a*a == b || b*b == a)
+    {
+        if (a*a == b) Console.WriteLine("Первое число является квадратом второго");
+        if (b*b == a) Console.WriteLine("Второе число является квадратом первого");
+    }
+    else Console.WriteLine("Ни первое число не является квадратом второго, ни наоборот");
+}
+Console.WriteLine("Введите первое число:");
+int First = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int Second = Convert.ToInt32(Console.ReadLine());
+Proverka(First, Second);
